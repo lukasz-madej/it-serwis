2b9301d8f49381343ec640116036745eda2ba467
lukasz-madej <lukasz.madej@kreuzwerker.pl>
2017-12-21 21:26:17 +0100

    Adds RWD and content to slider

--- js/it-serwis.js
+++ js/it-serwis.js
@@ -1,19 +1,26 @@
 $(() => {
   let $slider = $('#slider'),
-      $sliderBlue = $('#slider-blue'),
       $sliderRed = $('#slider-red');
 
-  $slider.find('.slider-collapse').on('click', () => {
+  $slider.find('.slider-collapse').on('click', (e) => {
+    e.preventDefault();
+    $sliderRed.removeClass('slider-expanded');
     $slider.find('.show-expanded').hide();
-    $slider.find('.show-collapsed').show();
-    $sliderBlue.css('width', '85%');
-    $sliderRed.css('width', '15%');
+    $slider.find('.hide-after-expanded').show();
+    $sliderRed.on('transitionend', () => {
+      $slider.find('.show-collapsed').show();
+      $sliderRed.off('transitionend');
+    });
   });
 
-  $slider.find('.slider-expand').on('click', () => {
+  $slider.find('.slider-expand').on('click', (e) => {
+    e.preventDefault();
+    $sliderRed.addClass('slider-expanded');
     $slider.find('.show-collapsed').hide();
-    $slider.find('.show-expanded').show();
-    $sliderBlue.css('width', '50%');
-    $sliderRed.css('width', '50%');
+    $sliderRed.on('transitionend', () => {
+      $slider.find('.hide-after-expanded').hide();
+      $slider.find('.show-expanded').show();
+      $sliderRed.off('transitionend');
+    });
   });
 });
